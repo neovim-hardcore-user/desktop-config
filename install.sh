6d37ea490bd7be0ea4c35d68358c10958d5fd42a
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2023-02-21 00:39:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,27 @@ sudo pacman -S flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update --appstream && flatpak --user update
 
+sudo pacman -S firefox conky polybar pcmanfm conky kitty krita blender gimp inkscape libreoffice-fresh neovim telegram-desktop signal vlc audacity kdenlive obs-studio
+
+
+
+
+
+
+
+
+
+
 
 yay -S leftwm logisim okteta-git parallel-launcher airshipper libresprite jitsi-meet github-desktop
-sudo pacman -S firefox conky polybar pcmanfm conky kitty krita blender gimp inkscape libreoffice-fresh neovim telegram-desktop signal vlc audacity kdenlive obs-studio
+
+
+
+
+
+
+
+
+
+
+
